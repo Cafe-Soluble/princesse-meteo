379ac975103482771d0d205fc62763e7c00c8316
Jucidful - Acer2016 <jeanchri.garcia@gmail.com>
2024-03-10 18:09:28 +0100

    affichage lyrics sur localhost:5000/subtitles

--- routes.py
+++ routes.py
@@ -9,3 +9,12 @@ def index():
     # Obtenir la date actuelle
     today = datetime.date.today()
     return render_template("index.html", today=today)
+
+
+@app.route('/subtitles')
+def subtitles():
+    return render_template('subtitles.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
